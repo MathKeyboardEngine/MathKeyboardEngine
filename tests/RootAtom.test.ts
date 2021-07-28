30bb6aadb04cc5bbf09e57a8a30c20ef6e43eed2
SymboLinker <SymboLinker@gmail.com>
2021-07-28 22:27:37 +0200

    Refactor unit tests of SyntaxTreeRoot ("RootAtom")

--- tests/RootAtom.test.ts
+++ tests/RootAtom.test.ts
@@ -5,10 +5,8 @@ import { Placeholder } from '../src/SyntaxTreeComponents/Placeholders/Placeholde
 import { DeleteCurrent } from '../src/KeyboardEngine/Functions/DeleteCurrent'
 import { Insert } from '../src/KeyboardEngine/Functions/Insert';
 import { FractionAtom } from '../src/SyntaxTreeComponents/Atoms/FractionAtom'
-import { MoveRight } from '../src/KeyboardEngine/Functions/MoveRight';
-import { WritableAtom } from '../src/SyntaxTreeComponents/Atoms/Base/WritableAtom';
 
-describe('RootAtom', () =>
+describe('SyntaxTreeRoot', () =>
 {
   it('is a Placeholder', () =>
   {
@@ -16,6 +14,7 @@ describe('RootAtom', () =>
     assert.isNotNull(k.Current);
     assert.isTrue(k.Current instanceof Placeholder);
   });
+
   it('cannot be deleted', () =>
   {
     let k = new KeyboardMemory();
@@ -23,32 +22,23 @@ describe('RootAtom', () =>
     assert.isNotNull(k.Current);
     assert.isTrue(k.Current instanceof Placeholder);
   });
-  it('is reachable via the chain of parents - from Placeholder', () =>
+
+  it('is reachable via the chain of parents', () =>
   {
     let k = new KeyboardMemory();
     let originalRoot = k.Current;
-    let fraction = new FractionAtom();
-    Insert(k, fraction);
-    assert.isTrue(originalRoot !== k.Current);
-    assert.isTrue(k.Current == fraction.Numerator)
+
+    let fraction1 = new FractionAtom();
+    Insert(k, fraction1);
+    assert.isTrue(k.Current === fraction1.Numerator)
+
+    let fraction2 = new FractionAtom();
+    Insert(k, fraction2);
+    assert.isTrue(k.Current === fraction2.Numerator)
+
     assert.isTrue(k.Current instanceof Placeholder)
-    let root = (k.Current as Placeholder).ParentAtom.ParentPlaceholder;
-    assert.isNull(root.ParentAtom);
-    expect(originalRoot).to.equal(root);
-  });
-  it('is reachable via the chain of parents - from Atom', () =>
-  {
-    let k = new KeyboardMemory();
-    let originalRoot = k.Current;
-    let fraction = new FractionAtom();
-    Insert(k, fraction);
-    MoveRight(k);
-    MoveRight(k);
-    assert.isTrue(originalRoot !== k.Current);
-    assert.isTrue(k.Current == fraction)
-    assert.isTrue(k.Current instanceof WritableAtom)
-    let root = (k.Current as WritableAtom).ParentPlaceholder;
-    assert.isNull(root.ParentAtom);
-    expect(originalRoot).to.equal(root);
+    let calculatedRoot = (k.Current as Placeholder).ParentAtom.ParentPlaceholder.ParentAtom.ParentPlaceholder;
+    assert.isNull(calculatedRoot.ParentAtom);
+    expect(originalRoot).to.equal(calculatedRoot);
   });
 });
