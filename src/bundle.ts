812b3ee6753c3de41287253f7888f15463acb343
SymboLinker <SymboLinker@gmail.com>
2021-08-09 22:37:03 +0200

    Build: create single-file-library in formats iife & esm

---
+++ src/bundle.ts
@@ -0,0 +1,74 @@
+import { DeleteCurrent } from "./KeyboardEngine/Functions/Delete/DeleteCurrent";
+export {DeleteCurrent};
+
+import { DeleteSelection } from "./KeyboardEngine/Functions/Delete/DeleteSelection";
+export {DeleteSelection};
+
+import { Insert } from "./KeyboardEngine/Functions/Insert/Insert";
+export {Insert};
+
+import { TryInsertWithEncapsulateCurrent } from "./KeyboardEngine/Functions/Insert/TryInsertWithEncapsulateCurrent";
+export {TryInsertWithEncapsulateCurrent};
+
+import { TryInsertWithEncapsulateSelection } from "./KeyboardEngine/Functions/Insert/TryInsertWithEncapsulateSelection";
+export {TryInsertWithEncapsulateSelection};
+
+import { MoveDown } from "./KeyboardEngine/Functions/Navigation/MoveDown";
+export{MoveDown};
+
+import { MoveLeft } from "./KeyboardEngine/Functions/Navigation/MoveLeft";
+export {MoveLeft};
+
+import { MoveRight } from "./KeyboardEngine/Functions/Navigation/MoveRight";
+export {MoveRight};
+
+import { MoveUp } from "./KeyboardEngine/Functions/Navigation/MoveUp";
+export {MoveUp};
+
+import { LeaveSelectionMode } from "./KeyboardEngine/Functions/Selection/LeaveSelectionMode";
+export {LeaveSelectionMode};
+
+import { SelectLeft } from "./KeyboardEngine/Functions/Selection/SelectLeft";
+export {SelectLeft};
+
+import { SelectRight } from "./KeyboardEngine/Functions/Selection/SelectRight";
+export {SelectRight};
+
+import { KeyboardMemory } from "./KeyboardEngine/KeyboardMemory";
+export {KeyboardMemory};
+
+import { DecimalSeparatorAtom } from "./SyntaxTreeComponents/Atoms/ReadonlyAtoms/DecimalSeparatorAtom";
+export {DecimalSeparatorAtom};
+
+import { DigitAtom } from "./SyntaxTreeComponents/Atoms/ReadonlyAtoms/DigitAtom";
+export {DigitAtom};
+
+import { RawAtom } from "./SyntaxTreeComponents/Atoms/ReadonlyAtoms/RawAtom";
+export {RawAtom};
+
+import { FractionAtom } from "./SyntaxTreeComponents/Atoms/WritableAtoms/FractionAtom";
+export {FractionAtom};
+
+import { MatrixAtom } from "./SyntaxTreeComponents/Atoms/WritableAtoms/MatrixAtom";
+export {MatrixAtom};
+
+import { NthRootAtom } from "./SyntaxTreeComponents/Atoms/WritableAtoms/NthRootAtom";
+export {NthRootAtom};
+
+import { PowerAtom } from "./SyntaxTreeComponents/Atoms/WritableAtoms/PowerAtom";
+export {PowerAtom};
+
+import { RoundBracketsAtom } from "./SyntaxTreeComponents/Atoms/WritableAtoms/RoundBracketsAtom";
+export {RoundBracketsAtom};
+
+import { SinglePlaceholderRawAtom } from "./SyntaxTreeComponents/Atoms/WritableAtoms/SinglePlaceholderRawAtom";
+export {SinglePlaceholderRawAtom};
+
+import { SubscriptAtom } from "./SyntaxTreeComponents/Atoms/WritableAtoms/SubscriptAtom";
+export {SubscriptAtom};
+
+import { Placeholder } from "./SyntaxTreeComponents/Placeholder/Placeholder";
+export {Placeholder};
+
+import {LatexConfiguration } from './LatexConfiguration';
+export {LatexConfiguration};
