bd42bd46b13677905a8181d6dc8f19029a82247f
SymboLinker <SymboLinker@gmail.com>
2021-09-08 21:20:03 +0200

    Add DeleteCurrent tests - bump test coverage to 97.87%

--- tests/src/KeyboardEngine/Functions/Delete/DeleteCurrent.test.ts
+++ tests/src/KeyboardEngine/Functions/Delete/DeleteCurrent.test.ts
@@ -13,6 +13,10 @@ import { StandardLeafNode } from '../../../../../src/SyntaxTreeComponents/Nodes/
 import { DecimalSeparatorNode } from '../../../../../src/SyntaxTreeComponents/Nodes/LeafNodes/DecimalSeparatorNode';
 import { InsertWithEncapsulateSelectionAndPrevious } from '../../../../../src/KeyboardEngine/Functions/Insert/InsertWithEncapsulateSelectionAndPrevious';
 import { SelectLeft } from '../../../../../src/KeyboardEngine/Functions/Selection/SelectLeft';
+import { DescendingBranchingNode } from '../../../../../src/SyntaxTreeComponents/Nodes/BranchingNodes/DescendingBranchingNode';
+import { MoveRight } from '../../../../../src/KeyboardEngine/Functions/Navigation/MoveRight';
+import { MatrixNode } from '../../../../../src/SyntaxTreeComponents/Nodes/BranchingNodes/MatrixNode';
+import { MoveLeft } from '../../../../../src/KeyboardEngine/Functions/Navigation/MoveLeft';
 
 describe(DeleteCurrent.name, () => {
   it('can also be used to "delete empty placeholders in some cases" (in the experience of the user) - x', () => {
@@ -137,8 +141,70 @@ describe(DeleteCurrent.name, () => {
     MoveDown(k);
     expectLatex('{◼}^{12}', k);
 
-    // Act & assert
+    // Act
     DeleteCurrent(k);
+    // Assert
     expectLatex('12◼', k);
+  });
+
+  it('Delete from right of BranchingNode - last placeholder filled"', () => {
+    // Arrange
+    const k = new KeyboardMemory();
+    Insert(k, new DigitNode('1'));
+    InsertWithEncapsulateCurrent(k, new DescendingBranchingNode(String.raw`\frac{`, '}{', '}'));
+    Insert(k, new StandardLeafNode('x'));
+    MoveRight(k);
+    expectLatex(String.raw`\frac{1}{x}◼`, k);
+    // Act
+    DeleteCurrent(k);
+    // Assert
+    expectLatex(String.raw`\frac{1}{◼}`, k);
+  });
+
+  it('Delete from right of BranchingNode - last placeholder empty and first contains 1 node"', () => {
+    // Arrange
+    const k = new KeyboardMemory();
+    Insert(k, new DigitNode('1'));
+    InsertWithEncapsulateCurrent(k, new DescendingBranchingNode(String.raw`\frac{`, '}{', '}'));
+    MoveRight(k);
+    MoveRight(k);
+    expectLatex(String.raw`\frac{1}{◻}◼`, k);
+    // Act
+    DeleteCurrent(k);
+    // Assert
+    expectLatex(String.raw`\frac{◼}{◻}`, k);
+  });
+
+  it('Delete from right of BranchingNode - last placeholder empty and first contains multiple nodes"', () => {
+    // Arrange
+    const k = new KeyboardMemory();
+    Insert(k, new DigitNode('1'));
+    Insert(k, new DigitNode('2'));
+    InsertWithEncapsulateCurrent(k, new DescendingBranchingNode(String.raw`\frac{`, '}{', '}'));
+    MoveRight(k);
+    MoveRight(k);
+    expectLatex(String.raw`\frac{12}{◻}◼`, k);
+    // Act
+    DeleteCurrent(k);
+    // Assert
+    expectLatex(String.raw`\frac{1◼}{◻}`, k);
+  });
+
+  it('Does nothing at deletion from first placeholder if multiple sibling placeholders are filled"', () => {
+    // Arrange
+    const k = new KeyboardMemory();
+    Insert(k, new MatrixNode('pmatrix', 2, 2));
+    MoveRight(k);
+    Insert(k, new DigitNode('2'));
+    MoveDown(k);
+    Insert(k, new DigitNode('4'));
+    MoveLeft(k);
+    MoveLeft(k);
+    MoveUp(k);
+    expectLatex(String.raw`\begin{pmatrix}◼ & 2 \\ ◻ & 4\end{pmatrix}`, k);
+    // Act
+    DeleteCurrent(k);
+    // Assert
+    expectLatex(String.raw`\begin{pmatrix}◼ & 2 \\ ◻ & 4\end{pmatrix}`, k);
   });
 });
